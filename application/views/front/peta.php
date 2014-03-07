b75f3bd5a55fac0c6bb638f0af8cc813f379d6e9
masjo91 <jokwon91@gmail.com>
2014-03-07 11:00:40 +0700

    update

--- application/views/front/peta.php
+++ application/views/front/peta.php
@@ -1,20 +1,70 @@
 <style type="text/css">
-  #map-canvas { height: 300px; }
+  #map-canvas { height: 550px; }
 </style>
 <script type="text/javascript"
   src="https://maps.googleapis.com/maps/api/js?key=AIzaSyABJD9IIW_lEgd8azMKO4YS-GfF7T7weuk&sensor=false">
 </script>
 <script type="text/javascript">
-  function initialize() {
-    var mapOptions = {
-      center: new google.maps.LatLng(-34.397, 150.644),
-      zoom: 8
-    };
-    var map = new google.maps.Map(document.getElementById("map-canvas"),
-        mapOptions);
-  }
-  google.maps.event.addDomListener(window, 'load', initialize);
-</script>
+   var map;
+   var markers = [];
+
+    function initialize() {
+      var myOptions = {
+        zoom: 16,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+      map = new google.maps.Map(document.getElementById('map-canvas'),
+          myOptions);
+
+      // Try HTML5 geolocation
+      if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          var pos = new google.maps.LatLng(position.coords.latitude,
+                                           position.coords.longitude);
+
+        
+
+          placeMarker(pos, 'Lokasi Anda');
+
+          map.setCenter(pos);
+        }, function() {
+          handleNoGeolocation(true);
+        });
+      } else {
+        // Browser doesn't support Geolocation
+        handleNoGeolocation(false);
+      }
+    }
+
+    function handleNoGeolocation(errorFlag) {
+      if (errorFlag) {
+        var content = 'Error: The Geolocation service failed.';
+      } else {
+        var content = 'Error: Your browser doesn\'t support geolocation.';
+      }
+
+      var options = {
+        map: map,
+        position: new google.maps.LatLng(60, 105),
+        content: content
+      };
+
+      var infowindow = new google.maps.InfoWindow(options);
+      map.setCenter(options.position);
+    }
+
+    function placeMarker(location, judul) {
+        var marker = new google.maps.Marker({
+            position: location, 
+            map: map,
+            title: judul
+        });
+
+        markers.push(marker);
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+  </script>
 
 
 <title><?= $title ?></title>
